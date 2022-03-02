97c08ffcf75dd74c026e31979a9d0471bbc57609
Евгений Сишненко <sishnenko86@mail.ru>
2022-03-02 23:05:04 +0300

    added loyaut

--- src/modules/todo/index.tsx
+++ src/modules/todo/index.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
 import { CardHead } from "./ui/CardHead";
 import { CardInput } from "./ui/CardInput";
+import { CardList } from "./ui/CardList";
+import Box from "@mui/material/Box";
 
 export const Todo = () => {
   return (
-    <Card sx={{ minWidth: "100%" }}>
-      <CardContent>
-        <CardHead />
-        <CardInput />
-        <CardList />
-      </CardContent>
-    </Card>
+    <Box sx={{ marginTop: "40px" }}>
+      <Card sx={{ minWidth: "100%" }}>
+        <CardContent>
+          <CardHead />
+          <CardInput />
+          <CardList />
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
