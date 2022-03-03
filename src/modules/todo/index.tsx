7646d722824353e7ef4de49c2403d11cb2646df6
Евгений Сишненко <sishnenko86@mail.ru>
2022-03-03 12:17:06 +0300

    delete useEffect

--- src/modules/todo/index.tsx
+++ src/modules/todo/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import { CardHead } from "./ui/CardHead";
@@ -9,11 +9,6 @@ import Box from "@mui/material/Box";
 import { todoApi } from "./api";
 
 export const Todo = () => {
-  useEffect(() => {
-    const res = todoApi({ method: "GET", body: "" });
-    console.log(res);
-  }, []);
-
   return (
     <Box sx={{ marginTop: "40px" }}>
       <Card sx={{ minWidth: "100%" }}>
